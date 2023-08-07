2608b4cabe9e87577d7f0e5872926527d4f449ca
tigerlab <tiggers12@hotmail.com>
2023-08-07 16:45:27 +0700

    ok

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,11 +26,7 @@ export default function RootLayout({ children }) {
           <Header />
           <Navbar />
           {children}
-          <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
-          />
+          <Footer year="2023" fullName="Tiger Tanner" studentId="650612083" />
         </div>
       </body>
     </html>
